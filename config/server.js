37048aab5dda1116f934b4de1c2484207abe8c65
Nguyen Cong Dat <nguyencongdat@Nguyens-MacBook-Air.local>
2022-12-17 11:47:39 +0700

    change env for dev

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  url: "http://13.214.190.50",
-  port: env.int('PORT', 1340),
+  // url: "http://13.214.190.50",
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
